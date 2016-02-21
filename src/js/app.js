b699486df59f04c3f83b158ed03a4e5a6c0a4381
Roel Noback <roel@noback.com>
2016-02-22 00:00:03 +0100

    main page setup / updates quiz

--- src/js/app.js
+++ src/js/app.js
@@ -25,70 +25,11 @@ mwqApp.config(function ($routeProvider) {
         templateUrl: 'html/pages/quiz.html',
         controller: 'quizController'
     })
-    /*
-    .when('/area', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'areaController'
-    })
-    .when('/volume', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'volumeController'
-    })
-     .when('/dry-volume', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'dryVolumeController'
-    })
-    .when('/mass', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'massController'
-    })
-    .when('/power', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'powerController'
-    })
-    .when('/energy', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'energyController'
-    })
-    .when('/force', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'forceController'
-    })
-    .when('/temperature', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'temperatureController'
-    })
-    .when('/velocity', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'velocityController'
-    })
-    .when('/acceleration', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'accelerationController'
-    })    
-    .when('/time', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'timeController'
-    })   
-    .when('/data', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'dataController'
-    })
-    .when('/angle', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'angleController'
-    })    
 
-    .when('/currency', {
-        templateUrl: 'html/pages/currency.html',
-        controller: 'currencyController'
+    .when('/info', {
+        templateUrl: 'html/pages/info.html',
+        controller: 'infoController'
     })
-    
-    .when('/converter', {
-        templateUrl: 'html/pages/default.html',
-        controller: 'converterController'
-    });
-    */
 });
 
 
@@ -116,9 +57,13 @@ mwqApp.config(function ($routeProvider) {
     });
 
     $(".nav-btn").on('click', function(evt){
+        $(".nav-btn").removeClass('active');
+        target = $(evt.target);
+        target.addClass('active');
+
         TweenMax.to($('#main-nav-mobile'), .5, {top: -180, ease:Expo.easeInOut}); 
         $scope.mobNavVisible = false;
-         $( "#nav-toggle" ).removeClass( "active" );
+        $( "#nav-toggle" ).removeClass( "active" );
     });
 
 	
