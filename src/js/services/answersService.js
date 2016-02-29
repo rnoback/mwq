747e17039d31a7754edf4c5ad080df80cec8c55c
Roel Noback <roel@noback.com>
2016-02-29 23:13:01 +0100

    connect answer array to firebase with timestamp

--- src/js/services/answersService.js
+++ src/js/services/answersService.js
@@ -5,11 +5,9 @@
         var self = this;
 
         this.storyStringBuild = "";
-        this.storyStringBuildEmail = "";
 
         this.clearMusicData = function(){
             self.storyStringBuild = "";
-            self.storyStringBuildEmail = "";
 
             self.classical = [];
             self.easy_jazz = [];
@@ -221,9 +219,9 @@
 
                         self.storyStringBuild = "<h2>" + self.Q1_headers[currentAnswer] + "</h2>";
                         self.storyStringBuild += "<p>" + self.Q1[currentAnswer] + "</p>";
-                        self.storyStringBuildEmail = self.Q1_headers[currentAnswer].toUpperCase();
-                        self.storyStringBuildEmail += "%0A" + self.Q1[currentAnswer];
+
                         break;
+
 
                     case 2: // question 2
                         if(currentAnswer === "A") {
@@ -302,7 +300,6 @@
 
                         }
                         self.storyStringBuild += "<p>" + self.Q2[currentAnswer] + "</p>";
-                        self.storyStringBuildEmail += "%0A" + self.Q2[currentAnswer];
                         break;
 
                         
@@ -382,7 +379,6 @@
                             this.kids_hits.push('3E');
                         }
                         self.storyStringBuild += "<p>" + self.Q3[currentAnswer] + "</p>";
-                        self.storyStringBuildEmail += "%0A"+self.Q3[currentAnswer];
                         break;
                     case 4:
                         if(currentAnswer === "A") {
@@ -455,7 +451,6 @@
                            
                         }
                         self.storyStringBuild += "<p>" + self.Q4[currentAnswer] + "</p>";
-                        self.storyStringBuildEmail += "%0A" + self.Q4[currentAnswer];
                         break;
                     case 5:
                         if(currentAnswer === "A") {
@@ -514,7 +509,6 @@
 
                         }
                         self.storyStringBuild += "<p>" + self.Q5[currentAnswer] + "</p>";
-                        self.storyStringBuildEmail += "%0A" + self.Q5[currentAnswer];
                         break;
                     case 6:
                         if(currentAnswer === "A") {
@@ -580,7 +574,6 @@
                            
                         }
                         self.storyStringBuild += "<p>" + self.Q6[currentAnswer] + "</p>";
-                        //self.storyStringBuildEmail += "%0A" + self.Q6[currentAnswer];
                         break;
                     case 7:
                         if(currentAnswer === "A") {
@@ -674,7 +667,6 @@
 
                         }
                         self.storyStringBuild += "<p>" + self.Q7[currentAnswer] + "</p>";
-                        //self.storyStringBuildEmail += "%0A" + self.Q7[currentAnswer];
                         break;
                     case 8:
                         if(currentAnswer === "A") {
@@ -739,17 +731,7 @@
 
                         }
                         self.storyStringBuild += "<p>" + self.Q8[currentAnswer] + "</p>";
-                        //self.storyStringBuildEmail += "%0A" + self.Q8[currentAnswer];
                         $('.answer-output-text').html(self.storyStringBuild);
-
-                        this.doMailto = function() {
-                            var sMailto= "mailto:info@muziekwerkt.nl?SUBJECT=Muziek Advies&BODY="+  self.storyStringBuildEmail;
-                            document.location.href = sMailto;
-                        }
-
-                        //$(window).storyStringBuild = self.storyStringBuild;
-                        $('.btn-email-advies').attr("href", self.sMailto() );
-
                         break;
 
                     default:
